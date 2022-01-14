81e93d08f9722ed10cb6a2b84a30f16ac76e20ac
evpronina <pronina.ekaterina@eurostavka.ru>
2022-01-15 02:22:27 +0300

    create markup

---
+++ src/tutorial-2/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <form className="form">
+      <input type="text" placeholder="E-mail" className="input" />
+      <input type="password" placeholder="Пароль" className="input" />
+      <button className="button">Войти</button>
+    </form>
+  );
+}
+
+export default App;
