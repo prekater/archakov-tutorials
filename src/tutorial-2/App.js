37c2ada0a0a7d4d608cc8f6801dc1d662540ab60
evpronina <pronina.ekaterina@eurostavka.ru>
2022-01-15 02:41:44 +0300

    add hadler for inputs

--- src/tutorial-2/App.js
+++ src/tutorial-2/App.js
@@ -1,9 +1,42 @@
 function App() {
+  let email, password;
+
+  function handleChangeInput(event) {
+    switch (event.target.name) {
+      case 'email':
+        email = event.target.value;
+        break;
+      case 'password':
+        password = event.target.value;
+        break;
+    }
+    // console.log('email', email);
+    // console.log('password', password);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('handleSubmit was called');
+  }
+
   return (
-    <form className="form">
-      <input type="text" placeholder="E-mail" className="input" />
-      <input type="password" placeholder="Пароль" className="input" />
-      <button className="button">Войти</button>
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="E-mail"
+        className="input"
+        onChange={handleChangeInput}
+        name="email"
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        className="input"
+        onChange={handleChangeInput}
+        name="password"
+      />
+      <button className="button" type="submit">
+        Войти
+      </button>
     </form>
   );
 }
